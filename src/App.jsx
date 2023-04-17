3bbb310079285139d367e9d13ebaab7f3fe8dd6a
P1l3T0 <vgan3v@gmail.com>
2023-04-17 22:44:10 +0300

    added more UI

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function Board() {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
 
-            if (squares[a] && squares[a] === squares[b] && squares[c])
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
                 return squares[a];
         }
 
